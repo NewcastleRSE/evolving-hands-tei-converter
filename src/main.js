4852ff3592d5b1d44c88317560718689a48b1995
Tiago Sousa Garcia <tiagosousagarcia@gmail.com>
2023-12-14 09:16:43 +0000

    test component export working fine

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
-import './lib/TestComponent.svelte'
 import App from './App.svelte'
 
 // const app = new App({
 //   target: document.getElementById('app'),
 // })
 
-export default app
+export default App
--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,19 @@ export default defineConfig({
       customElement: true,
     }
   })],
+  build: {
+    target: "esnext",
+    rollupOptions: {
+      input: {
+        index: "./src/main.js",
+      },
+      // single
+      output: {
+        format: "umd",
+        chunkFileNames: `[name].[hash].js`,
+        entryFileNames: "test-component.umd.js",
+        dir: "dist",
+      },
+    },
+  }
 })
