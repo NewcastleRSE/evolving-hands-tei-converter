a22da2ac5126775edf3aa636259483d52f5f5c4b
Tiago Sousa Garcia <tiagosousagarcia@gmail.com>
2023-12-15 09:57:04 +0000

    default css is now bundled with the component

--- src/App.svelte
+++ src/App.svelte
@@ -1,36 +1,38 @@
-<svelte:options customElement={{tag: 'test-component', shadow: 'none'}}/>
+<svelte:options customElement={{ tag: "test-component", shadow: "none" }} />
 
 <script>
-    import { onMount } from 'svelte';
-    import  CETEI  from 'CETEIcean';
+    import { onMount } from "svelte";
+    import CETEI from "CETEIcean";
 
-    export let path='https://raw.githubusercontent.com/NewcastleRSE/beeing-human-tei-data/dev/1623_consolidated.xml'
+    export let path = "";
     let error = undefined;
     let loaded = false;
 
     onMount(async () => {
         try {
-            if (path === '') {
-                throw 'No path specified';
+            if (path === "") {
+                throw "No path specified";
             }
             var cetei = new CETEI();
             console.log(cetei);
             // cetei.addBehaviors(teiBehaviours);
-            cetei.getHTML5(path, function(data) {
-                document.getElementById('TEI-container').appendChild(data);
+            cetei.getHTML5(path, function (data) {
+                document.getElementById("TEI-container").appendChild(data);
             });
             loaded = true;
         } catch (err) {
             error = err.toString();
             loaded = true;
-            return
+            return;
         }
-    })
-
-
+    });
 </script>
 
-<div id='TEI-container' data-testid="TEI-container">
+<!-- <svelte:head>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+</svelte:head> -->
+
+<div id="TEI-container" data-testid="TEI-container">
     {#if error}
         <p data-testid="error-message">{error}</p>
     {/if}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import App from './App.svelte'
+import '../static/style.css'
 
 // const app = new App({
 //   target: document.getElementById('app'),
