f5f32344eb1ce141f2c493aa17f4fbefec87e948
tiagosousagarcia <tiagosousagarcia@gmail.com>
2024-06-27 11:28:18 +0100

    corrected an empty import

--- static/behavioursDictionary.js
+++ static/behavioursDictionary.js
@@ -1,4 +1,4 @@
-import { addNoteToDiv, extractNotes, formatPagePoints, formatPoints, generateNoteLink, getNamedEntitiesData, noteToEvent, transformNamedEntityLink, replaceChoiceEltWithMarker, replaceChoiceWithEvent, removeDuplicateDatesWorkaround, addMarkersToElement, getFigDesc, addFigAbs, addFigDescToFootnoteDiv } from "../src/utils/auxFunctions";
+import { addNoteToDiv, extractNotes, formatPagePoints, formatPoints, generateNoteLink, getNamedEntitiesData, noteToEvent, transformNamedEntityLink, replaceChoiceEltWithMarker, replaceChoiceWithEvent, removeDuplicateDatesWorkaround, addMarkersToElement, getFigDesc, addFigAbs } from "../src/utils/auxFunctions";
 
 export let behaviours = function (options) {
     return {
