7ed0fd59f5154759e85ba8ab16a034e35ee94620
tiagosousagarcia <tiagosousagarcia@gmail.com>
2024-07-26 09:07:35 +0100

    corrected one problem, deleted unnecessary console logs

--- src/App.svelte
+++ src/App.svelte
@@ -55,70 +55,75 @@
                     // checks to see if the pageRange is valid
                     if (isNaN(pageRange)) {
                         try {
-                            console.log('entered')
-                            page_range = pageRange.split('-');
-                            console.log(page_range);
-                            if (!isNaN(page_range[0]) && !isNaN(page_range[1])) {
+                            page_range = pageRange.split("-");
+                            if (
+                                !isNaN(page_range[0]) &&
+                                !isNaN(page_range[1])
+                            ) {
                                 page_range[0] = parseInt(page_range[0]) - 1;
                                 page_range[1] = parseInt(page_range[1]);
                             } else {
-                                throw new Error('Page range must be in format x-y, where x and y are numbers');
+                                throw new Error(
+                                    "Page range must be in format x-y, where x and y are numbers",
+                                );
                             }
                         } catch (e) {
-                            console.error(e)
-                            page_range = 'all'
+                            console.error(e);
+                            page_range = "all";
                         }
                     } else {
                         // page range is a single number
-                        page_range = [parseInt(pageRange) -1, parseInt(pageRange)]
+                        page_range = [
+                            parseInt(pageRange) - 1,
+                            parseInt(pageRange),
+                        ];
                     }
 
-                    // Needs test here to make sure it's in range
+                    // Tests that the page range is in bounds
                     const pages = data.getElementsByTagName("tei-pb");
                     if (page_range[1] > pages.length) {
-                        console.error(`Page range is out of bounds: document only has ${pages.length} pages`);
-                        page_range = 'all'
+                        console.error(
+                            `Page range is out of bounds: document only has ${pages.length} pages`,
+                        );
+                        page_range = "all";
                     }
 
-                    if (page_range[0] < 1) {
-                        console.error(`Page numbers start at 1, not ${page_range[0]+1}`);
-                        page_range = 'all'
+                    if (page_range[0] < 0) {
+                        console.error(
+                            `Page numbers start at 1, not ${page_range[0] + 1}`,
+                        );
+                        page_range = "all";
                     }
 
-                    if (page_range === 'all') {
-                        page_range = [0, pages.length - 1]
-                    } 
-                    
-                    console.log(pages.length, page_range);
+                    if (page_range != "all") {
+                        // defines starting point for pagination
+                        let nextSib = pages[page_range[0]];
 
-                    // defines starting point for pagination
-                    let nextSib = pages[page_range[0]];
+                        // creates the new div to be shown
+                        const newBodyDiv = document.createElement("div");
 
-                    // creates the new div to be shown
-                    const newBodyDiv = document.createElement("div");
-
-                    // collects list of elements that need to be added - this collection needs to be separated from the moving of the element to avoid having to deep clone it, which will fail the custom event (though I think it shouldn't)
-                    const elToAdd = [];
-                    while (nextSib != pages[page_range[1]]) {
-                        console.log(nextSib)
-                        elToAdd.push(nextSib);
-                        try {
-                            nextSib = nextSib.nextSibling;
-                        } catch (e) {
-                            // reached the end of the array
-                            console.log('reached the end of the document')
-                            break;
+                        // collects list of elements that need to be added - this collection needs to be separated from the moving of the element to avoid having to deep clone it, which will fail the custom event (though I think it shouldn't)
+                        const elToAdd = [];
+                        while (nextSib != pages[page_range[1]]) {
+                            elToAdd.push(nextSib);
+                            try {
+                                nextSib = nextSib.nextSibling;
+                            } catch (e) {
+                                // reached the end of the array
+                                console.log("reached the end of the document");
+                                break;
+                            }
                         }
-                    }
 
-                    // creates the new body with just the required pages
-                    for (const el of elToAdd) {
-                        newBodyDiv.append(el);
-                    }
+                        // creates the new body with just the required pages
+                        for (const el of elToAdd) {
+                            newBodyDiv.append(el);
+                        }
 
-                    // replaces the entire body with only the selected pages
-                    const body = data.getElementsByTagName("tei-body")[0];
-                    body.replaceChildren(newBodyDiv);
+                        // replaces the entire body with only the selected pages
+                        const body = data.getElementsByTagName("tei-body")[0];
+                        body.replaceChildren(newBodyDiv);
+                    }
                 }
             });
             loaded = true;
