637f54e3f7acc67030ac3370b59b99f305522725
Tiago Sousa Garcia <tiagosousagarcia@gmail.com>
2025-02-21 11:41:53 +0000

    makes pagination reactive

--- src/App.svelte
+++ src/App.svelte
@@ -14,6 +14,10 @@
     export let path = "";
     export let configPath = "TeiConverter/TeiConverter.config.json";
     export let pageRange = undefined;
+
+    let xmlString = undefined;
+    let cetei = undefined;
+
     let error = undefined;
     let loaded = false;
     let config = undefined;
@@ -21,6 +25,115 @@
     let metadataFiles = undefined;
 
     let uniqueId = undefined;
+
+    function verifyPageRange(pageRange, data) {
+        let page_range = undefined;
+
+        if (isNaN(pageRange)) {
+            try {
+                page_range = pageRange.split("-");
+                if (!isNaN(page_range[0]) && !isNaN(page_range[1])) {
+                    page_range[0] = parseInt(page_range[0]) - 1;
+                    page_range[1] = parseInt(page_range[1]);
+                } else {
+                    throw new Error(
+                        "Page range must be in format x-y, where x and y are numbers",
+                    );
+                }
+            } catch (e) {
+                console.error(e);
+                return (page_range = "all");
+            }
+        } else {
+            // page range is a single number
+            return (page_range = [
+                parseInt(pageRange) - 1,
+                parseInt(pageRange),
+            ]);
+        }
+
+        // Tests that the page range is in bounds
+        const pages = data.getElementsByTagName("tei-pb");
+        if (page_range[1] > pages.length) {
+            console.error(
+                `Page range is out of bounds: document only has ${pages.length} pages`,
+            );
+            return (page_range = "all");
+        }
+
+        if (page_range[0] < 0) {
+            console.error(`Page numbers start at 1, not ${page_range[0] + 1}`);
+            return (page_range = "all");
+        }
+    }
+
+    function startPagination(pageRange, data = undefined) {
+        // decides whether the pagination is being started from witin the TEI transformation, or from the reactive variable
+        if (!data) {
+            // if there is no data, the function is being called from the reactive value; it will replace the existing HTML five with the original one
+            // this is necessary to ensure that pagination works properly, and that no custom behaviours are lost from deep cloning elements
+            try {
+                cetei.makeHTML5(xmlString, function (data) {
+                    let rootEl = document.getElementById(
+                        `TEI-container-${uniqueId}`,
+                    );
+                    rootEl.innerHTML = "";
+                    rootEl.appendChild(data);
+
+                    if (pageRange) {
+                    pagination(pageRange, data);
+                }
+                });
+            } catch (e) {
+                // it runs once on mount, so if it fails, it will try again on the next run
+                return;
+            }
+        } else {
+            pagination(pageRange, data);
+        }
+    }
+
+    function pagination(pageRange, data) {
+        // pagination - needs to happen after appending the data, otherwise all the behaviours will fail (the document will be empty)
+
+        if (pageRange) {
+            let page_range = undefined;
+            // checks to see if the pageRange is valid
+            page_range = verifyPageRange(pageRange, data);
+
+            const pages = data.getElementsByTagName("tei-pb");
+
+            if (page_range != "all") {
+                // defines starting point for pagination
+                let nextSib = pages[page_range[0]];
+
+                // creates the new div to be shown
+                const newBodyDiv = document.createElement("div");
+
+                // collects list of elements that need to be added - this collection needs to be separated from the moving of the element to avoid having to deep clone it, which will fail the custom event (though I think it shouldn't)
+                const elToAdd = [];
+                while (nextSib != pages[page_range[1]]) {
+                    elToAdd.push(nextSib);
+                    try {
+                        nextSib = nextSib.nextSibling;
+                    } catch (e) {
+                        // reached the end of the array
+                        console.log("reached the end of the document");
+                        break;
+                    }
+                }
+
+                // creates the new body with just the required pages
+                for (const el of elToAdd) {
+                    newBodyDiv.append(el);
+                }
+
+                // replaces the entire body with only the selected pages
+                const body = data.getElementsByTagName("tei-body")[0];
+                body.replaceChildren(newBodyDiv);
+            }
+        }
+    }
 
     onMount(async () => {
         uniqueId = Date.now();
@@ -51,114 +164,67 @@
                 if (config.metadataSeparate) {
                     metadataFiles = {};
                     for (let key of Object.keys(config.metadataSeparate)) {
-                        let xmlFile = await fetch(`${config.projectRoot}${config.metadataSeparate[key]}`);
+                        let xmlFile = await fetch(
+                            `${config.projectRoot}${config.metadataSeparate[key]}`,
+                        );
                         if (!xmlFile.ok) {
-                            throw new Error(`${config.projectRoot}${config.metadataSeparate[key]} does not exist`);
+                            throw new Error(
+                                `${config.projectRoot}${config.metadataSeparate[key]} does not exist`,
+                            );
                         } else {
                             let metadataFile = await xmlFile.text();
                             // parse the metadata file
                             let parser = new DOMParser();
-                            let xmlDoc = parser.parseFromString(metadataFile, "text/xml");
-                            metadataFiles[key] = xmlDoc
+                            let xmlDoc = parser.parseFromString(
+                                metadataFile,
+                                "text/xml",
+                            );
+                            metadataFiles[key] = xmlDoc;
                         }
                     }
                 }
             } catch (e) {
-                console.warn('Could not read the metadata file: ', e)
+                console.warn("Could not read the metadata file: ", e);
             }
 
-            var cetei = new CETEI({ignoreFragmentId: true});
+            cetei = new CETEI({ ignoreFragmentId: true });
             if (config.useCustomBehaviours) {
                 cetei.addBehaviors(teiBehaviours(config, metadataFiles));
             }
-            cetei.getHTML5(path, function (data) {
-                // show TEI document
-                document.getElementById(`TEI-container-${uniqueId}`).appendChild(data);
 
-                // pagination - needs to happen after appending the data, otherwise all the behaviours will fail (the document will be empty)
+            // separating the fetching from the transformation is necessary to allow for pagination
+            // fetch the TEI file
+            let xmlDoc = await fetch(path);
+            if (xmlDoc.ok) {
+                xmlString = await xmlDoc.text();
+            } else {
+                throw new Error(`${path} does not exist`);
+            }
+
+            // transform the TEI file
+            cetei.makeHTML5(xmlString, function (data) {
+                let rootEl = document.getElementById(
+                    `TEI-container-${uniqueId}`,
+                );
+                rootEl.innerHTML = "";
+                rootEl.appendChild(data);
+
                 if (pageRange) {
-                    let page_range = undefined;
-                    // checks to see if the pageRange is valid
-                    if (isNaN(pageRange)) {
-                        try {
-                            page_range = pageRange.split("-");
-                            if (
-                                !isNaN(page_range[0]) &&
-                                !isNaN(page_range[1])
-                            ) {
-                                page_range[0] = parseInt(page_range[0]) - 1;
-                                page_range[1] = parseInt(page_range[1]);
-                            } else {
-                                throw new Error(
-                                    "Page range must be in format x-y, where x and y are numbers",
-                                );
-                            }
-                        } catch (e) {
-                            console.error(e);
-                            page_range = "all";
-                        }
-                    } else {
-                        // page range is a single number
-                        page_range = [
-                            parseInt(pageRange) - 1,
-                            parseInt(pageRange),
-                        ];
-                    }
-
-                    // Tests that the page range is in bounds
-                    const pages = data.getElementsByTagName("tei-pb");
-                    if (page_range[1] > pages.length) {
-                        console.error(
-                            `Page range is out of bounds: document only has ${pages.length} pages`,
-                        );
-                        page_range = "all";
-                    }
-
-                    if (page_range[0] < 0) {
-                        console.error(
-                            `Page numbers start at 1, not ${page_range[0] + 1}`,
-                        );
-                        page_range = "all";
-                    }
-
-                    if (page_range != "all") {
-                        // defines starting point for pagination
-                        let nextSib = pages[page_range[0]];
-
-                        // creates the new div to be shown
-                        const newBodyDiv = document.createElement("div");
-
-                        // collects list of elements that need to be added - this collection needs to be separated from the moving of the element to avoid having to deep clone it, which will fail the custom event (though I think it shouldn't)
-                        const elToAdd = [];
-                        while (nextSib != pages[page_range[1]]) {
-                            elToAdd.push(nextSib);
-                            try {
-                                nextSib = nextSib.nextSibling;
-                            } catch (e) {
-                                // reached the end of the array
-                                console.log("reached the end of the document");
-                                break;
-                            }
-                        }
-
-                        // creates the new body with just the required pages
-                        for (const el of elToAdd) {
-                            newBodyDiv.append(el);
-                        }
-
-                        // replaces the entire body with only the selected pages
-                        const body = data.getElementsByTagName("tei-body")[0];
-                        body.replaceChildren(newBodyDiv);
-                    }
+                    pagination(pageRange, data);
                 }
             });
+
             loaded = true;
+
         } catch (err) {
             error = err.toString();
             loaded = true;
             return;
         }
     });
+
+    // pagination is reactive, each time the pageRange changes, the document is re-converted to HTML and paginated
+    $: startPagination(pageRange);
 </script>
 
 <div id="TEI-container-{uniqueId}" data-testid="TEI-container">
